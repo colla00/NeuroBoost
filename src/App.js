bfaff5baafebf91b92e6df616bcd0b365d4f8996
Portfolio of Projects by Alexis <info@alexiscollier.com>
2024-11-09 23:07:37 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>NeuroBoost: Cognitive Enhancement Platform</h1>
+            <p>Boost memory, focus, and cognitive skills with neuroscience-backed exercises.</p>
+        </div>
+    );
+}
+
+export default App;
